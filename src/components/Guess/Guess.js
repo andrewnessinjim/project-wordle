db19dcecd5382ccf52f27314b30827678b4b1f81
Andrew Nessin <andrewnessinjim@yahoo.com>
2023-10-29 08:05:09 +0530

    Exercise 4: Game logic - after solution video

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,14 +2,23 @@ import React from 'react';
 import { checkGuess } from '../../game-helpers';
 import { range } from '../../utils';
 
-function Guess({guess, answer}) {
+function Cell({letter, hint}) {
+  return (
+    <span className={`cell ${hint ?? ""}`}>
+      {letter}
+    </span>
+  )
+}
+function Guess({ guess, answer }) {
   const guessHints = checkGuess(guess, answer);
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className={`cell ${guessHints ? guessHints[num].status : ""}`}>
-          {guessHints ? guessHints[num].letter: undefined}
-        </span>
+        <Cell key={num}
+          hint={guessHints ? guessHints[num].status : undefined}
+          letter={guessHints ? guessHints[num].letter : undefined}
+          >
+        </Cell>
       ))}
     </p>
   );
